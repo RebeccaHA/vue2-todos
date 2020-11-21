8c5f413e7b17f06dd3f834c376ba97cdb1f80acc
RebeccaHA <rebeccahunt034@gmail.com>
2020-11-21 18:21:42 +0000

    removed vue.js script file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Router from "vue-router";
+import Home from "./components/Home";
 
-new Vue({
+const app = new Vue({
   render: h => h(App)
 }).$mount("#app");
+
+app.use(navigation);
+
+const navigation = new Router({ routes });
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  }
+];
