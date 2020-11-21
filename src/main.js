69e3f06ad79820bee320a1115a70da3558c339e5
RebeccaHA <rebeccahunt034@gmail.com>
2020-11-21 18:06:04 +0000

    set up form and display, working on fixing router issue

--- src/main.js
+++ src/main.js
@@ -1,21 +1,6 @@
 import Vue from "vue";
 import App from "./App.vue";
-import VueRouter from "vue-router";
-import Home from "./components/Home.vue";
 
 new Vue({
-  router,
   render: h => h(App)
 }).$mount("#app");
-
-const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home
-  }
-];
-
-const router = VueRouter({
-  routes
-});
